5c30d4e3bcf9655181c2aff7faaaf9a6bfdf9bae
MikroTik_2 <dotsenko2034@gmail.com>
2022-10-03 14:35:36 +0300

    hamburger

--- js/app.js
+++ js/app.js
@@ -1,7 +1,48 @@
-const hambururger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+//'use strict';
 
-hambururger.addEventListener("click", () => {
-   hambururger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-}) 
+//var number = 5;
+//var string = "Hello";
+//var sym = Symbol();
+//var boolean = true;     
+//null;
+//undefined;
+//var obj = {};
+//console.log(4/0);
+//console.log('string'*9);
+
+
+//let something;
+//console.log(something);
+
+
+////Операторы
+
+//let incr = 10,
+//    decr = 10;
+
+
+
+//console.log(incr++);
+//console.log(decr--);
+
+//console.log(5%2);
+//console.log("2" == 2);
+
+
+//let person = {
+//   name: "SFLJF",
+//   age: 23,
+//   isMarried: false,
+//}
+//console.table(person);
+
+let name = prompt("your name")
+
+let age = parseFloat(prompt("your age"))
+
+console.log(typeof age);
+
+let result = `howdy, my name ${name} age ${age}` 
+
+console.log(result);
+
